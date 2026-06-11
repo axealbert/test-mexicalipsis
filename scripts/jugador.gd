fb5528ec3a03b3ef4c336cf84079bc10eb55ab92
Axel Alberto González Fabián <axelalbertofabian@gmail.com>
2026-06-10 23:21:53 -0600

    change some functions for the animations of the character, logic

--- scripts/jugador.gd
+++ scripts/jugador.gd
@@ -1,29 +1,53 @@
 extends CharacterBody2D
 
 @onready var animation=$AnimatedSprite2D
-
-const SPEED = 200.0
-const JUMP_VELOCITY = -400.0
+@export var move_speed: int 
+@export var jump_speed: int
+var facing_right = true
 
 
 func _physics_process(delta: float) -> void:
 	
-	animation.play("idle")
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	update_animation()
+	move_x()
+	flip()
+	jump(delta)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		animation.play("run")
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
 
 	move_and_slide()
+	
+	
+func update_animation():
+	if not is_on_floor():
+		if velocity.y < 0:
+			animation.play("jump")
+		else:
+			animation.play("fall")
+		return
+	
+	
+	if velocity.x:
+		animation.play("run")
+	else:
+		animation.play("idle")
+		
+		
+func jump(delta):
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -jump_speed
+
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+func flip():
+	if (velocity.x < 0 && facing_right) or (not facing_right and velocity.x > 0):
+		scale.x *= -1
+		facing_right = not facing_right
+		animation.play("run")
+	
+func move_x():
+	var direction := Input.get_axis("move_left", "move_right")
+	velocity.x = direction * move_speed
+	
