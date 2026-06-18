f7a8e4f7102a0490195e46e4a5493eeda42649b8
Axel Alberto González Fabián <axelalbertofabian@gmail.com>
2026-06-17 23:49:14 -0600

    I'm with some bugs and trying to resolver why the collision doesn't work in the enemy and the killzone, it is taking a long sorry, I also advance with some animated platforms and the motion on the camera is know so smooth, also some changes in the design of the level

--- scripts/rat.gd
+++ scripts/rat.gd
@@ -1,14 +1,16 @@
 extends Area2D
 
-@onready var timer: Timer = $Timer
+@onready var timer_1: Timer = $Timer1
+
+	
 
 
 func _on_body_entered(body: Node2D) -> void:
-	print("moriste")
-	timer.start()
-		
+	print("moriste por la rata")
+	timer_1.start()
+	
+
 
 
 func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
-	
