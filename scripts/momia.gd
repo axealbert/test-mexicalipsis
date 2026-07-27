5a172952cc349ebc6c2aee30f7227103ab9b904e
Axel Alberto González Fabián <axelalbertofabian@gmail.com>
2026-07-27 16:15:58 -0600

    The enemy already works finally ;), i'm working on the design of the level and also i want to add a gun, working on its design

--- scripts/momia.gd
+++ scripts/momia.gd
@@ -13,35 +13,40 @@ extends CharacterBody2D
 
 var angle_rad
 var player
-var facing_right = true
+var facing_right = false
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("Jugador")
 	angle_rad = deg_to_rad(angle/2)
 	
 func _physics_process(delta: float) -> void:
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
 	# Ahorrar procesos físicos, cada 2 ciclos se activa el raycast
 	#if Engine.get_physics_frames() % 2 != 0:
 	#	return
-	
-	
 	if is_in_cone() and has_line_of_sight():
 		animated_sprite_2d.self_modulate = Color.RED
+		#if is_on_floor():
 		var direction = to_local(navigation_agent_2d.get_next_path_position()).normalized()
-		velocity = direction * speed
-		#if (velocity.x < 0 && facing_right) or (not facing_right and velocity.x > 0):
-		#	scale.x *= -1
-		#	facing_right = not facing_right
-		#animated_sprite_2d.play("run")
+		velocity.x = direction.x * speed
+		
+		if is_on_wall() and is_on_floor():
+			velocity.y = -200.0
+		
+		if (velocity.x < 0 && facing_right) or (not facing_right and velocity.x > 0):
+			animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h
+			facing_right = not facing_right
+	
+		animated_sprite_2d.play("run")
 	else:
 		animated_sprite_2d.self_modulate = Color.WHITE
 		animated_sprite_2d.play("idle")
 		velocity.x = 0
 		
 	move_and_slide()
-
+"""
 func _draw() -> void:
 	var left_dir = direction.rotated(-angle_rad)* length
 	var right_dir = direction.rotated(angle_rad) * length
@@ -52,7 +57,7 @@ func _draw() -> void:
 	var right_dir2 = direction2.rotated(angle_rad) * length
 	draw_line(Vector2.ZERO, left_dir2, Color.RED, 2.0)
 	draw_line(Vector2.ZERO, right_dir2, Color.RED, 2.0)
-	
+"""
 func is_in_cone():
 	var player_local_pos = to_local(player.global_position)
 	var dis = player_local_pos.length()
